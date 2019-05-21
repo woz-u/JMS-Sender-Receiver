8ad7c998bbf7571907d04714821faa88b683858c
Steve Bishop <xipooo@gmail.com>
2019-05-20 17:00:28 -0700

    Added POJOs project

---
+++ POJOs/src/com/exeter/pojos/ProductOrder.java
@@ -0,0 +1,33 @@
+package com.exeter.pojos;
+
+public class ProductOrder {
+	int productID;
+	String productName;
+	double quantity;
+	double price;
+	
+	public int getProductID() {
+		return productID;
+	}
+	public void setProductID(int productID) {
+		this.productID = productID;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public double getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(double quantity) {
+		this.quantity = quantity;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+}
